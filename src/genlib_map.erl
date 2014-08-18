2339e480c5cdd6887a99aa7522d85b55ce9674ed
Artem Ocheredko <aaocheredko@platbox.com>
2014-08-18 12:03:16 +0400

    Added missed genlib_map modifications

--- src/genlib_map.erl
+++ src/genlib_map.erl
@@ -8,9 +8,12 @@
 -export([get/2]).
 -export([get/3]).
 -export([foreach/2]).
+-export([truemap/2]).
 -export([compact/1]).
 -export([atomize/1]).
 -export([atomize/2]).
+-export([binarize/1]).
+-export([binarize/2]).
 
 %%
 
@@ -30,6 +33,12 @@ get(Key, Map = #{}, Default) ->
             Default
     end.
 
+-spec truemap(Function, map()) -> ok when
+    Function :: fun((Key :: any(), Value :: any()) -> {Key :: any(), Value :: any()}).
+
+truemap(F, Map = #{}) ->
+    maps:fold(fun (K, V, M) -> {Kn, Vn} = F(K, V), maps:put(Kn, Vn, M) end, #{}, Map).
+
 -spec foreach(Function, map()) -> ok when
     Function :: fun((Key :: any(), Value :: any()) -> any()).
 
@@ -44,7 +53,12 @@ compact(Map = #{}) ->
 -spec atomize(#{binary() => any()}) -> #{atom() => any()}.
 
 atomize(Map) ->
-    maps:fold(fun (K, V, M) -> maps:put(binary_to_atom(K, utf8), V, M) end, #{}, Map).
+    truemap(fun (K, V) -> {binary_to_atom(K, utf8), V} end, Map).
+
+-spec binarize(#{atom() => any()}) -> #{binary() => any()}.
+
+binarize(Map) ->
+    truemap(fun (K, V) -> {atom_to_binary(K, utf8), V} end, Map).
 
 -spec atomize(Map, pos_integer() | infinity) -> AtomicMap when
     Map       :: #{binary() => Map | term()},
@@ -55,6 +69,16 @@ atomize(Map, 1) ->
 
 atomize(Map, N) ->
     atomize(maps:map(fun (_, V) when is_map(V) -> atomize(V, decrement(N)); (_, V) -> V end, Map)).
+
+-spec binarize(AtomicMap, pos_integer() | infinity) -> Map when
+    Map       :: #{binary() => Map | term()},
+    AtomicMap :: #{atom() | binary() => AtomicMap | term()}.
+
+binarize(Map, 1) ->
+    binarize(Map);
+
+binarize(Map, N) ->
+    binarize(maps:map(fun (_, V) when is_map(V) -> binarize(V, decrement(N)); (_, V) -> V end, Map)).
 
 decrement(infinity) -> infinity;
 decrement(N) -> N - 1.
