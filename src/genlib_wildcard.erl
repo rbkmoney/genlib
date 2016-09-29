3bfc1eed5c478195aba073dbf3654a3b9b5ebe63
Andrey Mayorov <a.mayorov@rbkmoney.com>
2016-09-30 02:41:48 +0300

    Make wildcard escaping rules more consistent

--- src/genlib_wildcard.erl
+++ src/genlib_wildcard.erl
@@ -8,25 +8,20 @@
 ]).
 
 -spec match(binary(), binary()) -> boolean().
-match(Body, Pattern) when is_binary(Body) ->
-    match(Body, Pattern, true).
 
-match(Body, Pattern = <<"\\"/utf8, T/binary>>, true) ->
-    case T of
-        R = <<H/utf8, _/binary>> when H =:= $*; H =:= $?; H =:= $\\ ->
-            match(Body, R, false);
-        _ ->
-            match(Body, Pattern, false)
-    end;
-match(_Body, <<"*"/utf8>>, true) ->
-    true;
-match(Body = <<_/utf8, Rest/binary>>, Pattern = <<"*"/utf8, T/binary>>, true) ->
-    match(Body, T) orelse match(Rest, Pattern);
-match(<<_/utf8, Rest/binary>>, <<"?"/utf8, T/binary>>, true) ->
-    match(Rest, T);
-match(<<H/utf8, R/binary>>, <<H/utf8, T/binary>>, _) ->
+match(<<H/utf8, R/binary>>, <<"\\"/utf8, H/utf8, T/binary>>) ->
     match(R, T);
-match(<<>>, <<>>, _) ->
+match(Body = <<_/utf8, Rest/binary>>, Pattern = <<"*"/utf8, T/binary>>) ->
+    match(Body, T) orelse match(Rest, Pattern);
+match(<<_/utf8, Rest/binary>>, <<"?"/utf8, T/binary>>) ->
+    match(Rest, T);
+match(<<H/utf8, R/binary>>, <<H/utf8, T/binary>>) ->
+    match(R, T);
+match(_, <<"\\"/utf8, _/binary>>) ->
+    false;
+match(_, <<"*"/utf8>>) ->
     true;
-match(_, _, _) ->
+match(<<>>, <<>>) ->
+    true;
+match(_, _) ->
     false.
