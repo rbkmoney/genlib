02998eb643c9c6b49969fc77054c4925b883bf26
Andrew Mayorov <encube.ul@gmail.com>
2016-12-22 19:47:30 +0300

    Introduce naïve rational arithmetic (#3)
    * Introduce naïve rational arithmetic
    
    * Change rounding rule to the Commercial rounding
    
    * Add comparison and numerator / denominator accessors

---
+++ src/genlib_rational.erl
@@ -0,0 +1,116 @@
+%%%
+%%% Genlib
+%%% Rational number arithmetic subset
+%%%
+%%% # Rounding rule
+%%%
+%%% Commercial rounding [1], round up iff the remainder is not less than the
+%%% half of the denominator in terms of absolute value:
+%%%
+%%%     round(  5 /  3) =  2
+%%%     round( -5 /  3) = -2
+%%%     round(  7 /  5) =  1
+%%%     round( -7 /  5) = -1
+%%%     round( 15 / 10) =  2
+%%%     round(-15 / 10) = -2
+%%%
+%%% [1]: https://en.wikipedia.org/wiki/Rounding#Round_half_away_from_zero
+%%%
+
+-module(genlib_rational).
+
+%%
+
+-opaque t() :: {integer(), pos_integer()}.
+-export_type([t/0]).
+
+-export([new/1]).
+-export([new/2]).
+-export([num/1]).
+-export([denom/1]).
+-export([round/1]).
+
+-export([cmp/2]).
+
+-export([neg/1]).
+-export([add/2]).
+-export([sub/2]).
+-export([inv/1]).
+-export([mul/2]).
+-export([dvd/2]).
+
+-compile([no_auto_import]).
+
+%%
+
+-spec new(integer()) -> t().
+
+new(I) ->
+    {I, 1}.
+
+-spec new(integer(), neg_integer() | pos_integer()) -> t().
+
+new(P, Q) when Q > 0 ->
+    {P, Q};
+new(P, Q) when Q < 0 ->
+    {-P, -Q};
+new(_, 0) ->
+    erlang:error(badarg).
+
+-spec num(t()) -> integer().
+
+num({P, _}) ->
+    P.
+
+-spec denom(t()) -> neg_integer() | pos_integer().
+
+denom({_, Q}) ->
+    Q.
+
+-spec round(t()) -> integer().
+
+round({0, _}) ->
+    0;
+round({P, Q}) when P > 0 ->
+    P div Q + case 2 * (P rem Q) < Q of true -> 0; false -> 1 end;
+round({P, Q}) ->
+    -round({-P, Q}).
+
+-spec cmp(t(), t()) -> eq | gt | lt.
+
+cmp(R1, R2) ->
+    case num(sub(R1, R2)) of
+        P when P > 0 -> gt;
+        P when P < 0 -> lt;
+        0 -> eq
+    end.
+
+-spec neg(t()) -> t().
+
+neg({P, Q}) ->
+    new(-P, Q).
+
+-spec add(t(), t()) -> t().
+
+add({P1, Q1}, {P2, Q2}) ->
+    {P1 * Q2 + P2 * Q1, Q1 * Q2}.
+
+-spec sub(t(), t()) -> t().
+
+sub(R1, R2) ->
+    add(R1, neg(R2)).
+
+-spec inv(t()) -> t().
+
+inv({P, Q}) ->
+    new(Q, P).
+
+-spec mul(t(), t()) -> t().
+
+mul({P1, Q1}, {P2, Q2}) ->
+    {P1 * P2, Q1 * Q2}.
+
+-spec dvd(t(), t()) -> t().
+
+dvd(R1, R2) ->
+    mul(R1, inv(R2)).
---
+++ test/genlib_rational_tests.erl
@@ -0,0 +1,66 @@
+%%
+
+-module(genlib_rational_tests).
+
+-include_lib("eunit/include/eunit.hrl").
+
+-compile([no_auto_import]).
+
+-spec test() -> _.
+
+-type testcase() :: {_, fun()}.
+
+%%
+
+-spec general_test_() -> [testcase()].
+-spec compare_test_() -> [testcase()].
+-spec round_test_() -> [testcase()].
+
+general_test_() ->
+    [
+        ?_assertError(badarg, new(3, 0)),
+        ?_assertError(badarg, inv(new(0, -3))),
+
+        ?_assertEqual(-3, round(neg(new( -3,   -1)))),
+        ?_assertEqual(1 , round(add(new( 15,   31), new(  16,   31)))),
+        ?_assertEqual(2 , round(add(new(  5,    3), new(   3,    7)))),
+        ?_assertEqual(0 , round(sub(new( 17,    3), new(  17,    3)))),
+        ?_assertEqual(3 , round(inv(new( -1,   -3)))),
+        ?_assertEqual(1 , round(mul(new(  1,    3), new(   3)))),
+        ?_assertEqual(1 , round(mul(new( -1,   -3), new(   3)))),
+        ?_assertEqual(58, round(mul(new(333, 1000), new(1755,   10)))),
+        ?_assertEqual(1 , round(dvd(new(  5,    7), new(   2,    3)))),
+        ?_assertEqual(2 , round(dvd(new(  4,    7), new(   2,    7))))
+    ].
+
+compare_test_() ->
+    [
+        ?_assertEqual(eq, cmp(new( 17,  3), new( 17,   3))),
+        ?_assertEqual(eq, cmp(new(  1,  4), new( 25, 100))),
+        ?_assertEqual(gt, cmp(new(  1,  3), new(  1,   4))),
+        ?_assertEqual(lt, cmp(new(  1,  5), new(  1,   4)))
+    ].
+
+round_test_() ->
+    [
+        ?_assertEqual( 2, round(new(  5,  3))),
+        ?_assertEqual(-2, round(new( -5,  3))),
+        ?_assertEqual( 1, round(new(  7,  5))),
+        ?_assertEqual(-1, round(new( -7,  5))),
+        ?_assertEqual( 1, round(new(  7,  5))),
+        ?_assertEqual( 2, round(new( 15, 10))),
+        ?_assertEqual(-2, round(new(-15, 10)))
+    ].
+
+%%
+
+new(P)      -> genlib_rational:new(P).
+new(P, Q)   -> genlib_rational:new(P, Q).
+cmp(P, Q)   -> genlib_rational:cmp(P, Q).
+neg(R1)     -> genlib_rational:neg(R1).
+inv(R1)     -> genlib_rational:inv(R1).
+add(R1, R2) -> genlib_rational:add(R1, R2).
+sub(R1, R2) -> genlib_rational:sub(R1, R2).
+mul(R1, R2) -> genlib_rational:mul(R1, R2).
+dvd(R1, R2) -> genlib_rational:dvd(R1, R2).
+round(R1)   -> genlib_rational:round(R1).
