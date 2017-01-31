82ff16f4314fc406dd90752467a08fe401b009ef
Andrew Mayorov <encube.ul@gmail.com>
2017-01-31 18:08:39 +0300

    HG-145: Make shift_date able to handle complex date intervals (#5)

--- test/genlib_time_tests.erl
+++ test/genlib_time_tests.erl
@@ -1,6 +1,8 @@
 -module(genlib_time_tests).
 -include_lib("eunit/include/eunit.hrl").
 
+-type testcase() :: {_, fun()}.
+
 -spec test() -> _.
 
 -spec consistent_tz_test() -> _.
@@ -16,3 +18,33 @@ get_timezone_for(Mcs, T0) ->
         Diff when Diff > Mcs -> [];
         _Diff -> [genlib_time:get_timezone() | get_timezone_for(Mcs, T0)]
     end.
+
+-spec shift_date_test_() -> [testcase()].
+
+shift_date_test_() ->
+    [
+        ?_assertEqual(
+            {2018, 2, 28},
+            genlib_time:shift_date({2017, 1, 31}, {1, 1, 0})
+        ),
+        ?_assertEqual(
+            {2018, 3, 1},
+            genlib_time:shift_date({2017, 1, 31}, {1, 1, 1})
+        ),
+        ?_assertEqual(
+            {2016, 2, 29},
+            genlib_time:shift_date({2017, 1, 31}, {-2, 13, 0})
+        ),
+        ?_assertEqual(
+            {2016, 3, 1},
+            genlib_time:shift_date({2017, 1, 31}, {-2, 13, 1})
+        ),
+        ?_assertEqual(
+            {2018, 1, 31},
+            genlib_time:shift_date({2017, 1, 31}, {0, 0, 365})
+        ),
+        ?_assertEqual(
+            {2017, 2, 28},
+            genlib_time:shift_date({2016, 2, 29}, {0, 0, 365})
+        )
+    ].
