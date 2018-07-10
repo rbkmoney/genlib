160bbfe096cbbb45f344ce521120fc012b902f49
Petr Kozorezov <petr.kozorezov@gmail.com>
2018-07-10 18:04:57 +0300

    apply review comments

--- src/genlib_gen.erl
+++ src/genlib_gen.erl
@@ -5,7 +5,6 @@
 
 %% API
 -export_type([reason                /0]).
--export_type([timeout_              /0]).
 -export_type([start_ret             /0]).
 -export_type([ref                   /0]).
 -export_type([reg_name              /0]).
@@ -16,8 +15,10 @@
 -export_type([server_handle_info_ret/1]).
 -export_type([server_code_change_ret/1]).
 -export_type([supervisor_ret        /0]).
--export([reg_name2_ref/1]).
--export([where        /1]).
+-export([reg_name2_ref  /1]).
+-export([where          /1]).
+-export([reg_name_to_pid/1]).
+-export([ref_to_pid     /1]).
 
 %%
 %% API
@@ -29,11 +30,6 @@
     | _
 .
 
--type timeout_() ::
-      'hibernate'
-    | timeout()
-.
-
 -type start_ret() ::
       {ok, pid()}
     | ignore
@@ -42,7 +38,7 @@
 
 -type ref() ::
       atom()
-    | {atom(), atom()}
+    | {node(), atom()}
     | {global, atom()}
     | {via, atom(), term()}
     | pid()
@@ -59,28 +55,28 @@
        ignore
     | {ok  , State   }
     | {stop, reason()}
-    | {ok  , State   , timeout_()}
+    | {ok  , State   , timeout_() | hibernate}
 .
 
--type server_handle_call_ret(State) ::
+-type server_handle_call_ret(Reply, State) ::
       {noreply, State   }
-    | {noreply, State   , timeout_()   }
-    | {reply  , _Reply  , State        }
-    | {stop   , reason(), State        }
-    | {reply  , _Reply  , State        , timeout_()}
-    | {stop   , reason(), _Reply       , State     }
+    | {noreply, State   , timeout_() | hibernate}
+    | {reply  , Reply   , State                 }
+    | {stop   , reason(), State                 }
+    | {reply  , Reply   , State        , timeout_() | hibernate}
+    | {stop   , reason(), _Reply       , State                 }
 .
 
 -type server_handle_cast_ret(State) ::
       {noreply, State   }
-    | {noreply, State   , timeout_()}
-    | {stop   , reason(), State        }
+    | {noreply, State   , timeout_() | hibernate}
+    | {stop   , reason(), State                 }
 .
 
 -type server_handle_info_ret(State) ::
       {noreply, State   }
-    | {noreply, State   , timeout_()}
-    | {stop   , reason(), State     }
+    | {noreply, State   , timeout_() | hibernate}
+    | {stop   , reason(), State                 }
 .
 
 -type server_code_change_ret(State) ::
@@ -94,10 +90,10 @@
 .
 
 -spec
-reg_name2_ref(reg_name()) -> ref().
-reg_name2_ref({local, Name} ) -> Name;
-reg_name2_ref(V={global, _} ) -> V;
-reg_name2_ref(V={via, _, _} ) -> V.
+reg_name2_ref(reg_name()   ) -> ref().
+reg_name2_ref({local, Name}) -> Name;
+reg_name2_ref(V={global, _}) -> V;
+reg_name2_ref(V={via, _, _}) -> V.
 
 -spec where(reg_name()) ->
     pid().
@@ -107,3 +103,25 @@ where({global, Name}) ->
     global:whereis_name(Name);
 where({via, Module, Name}) ->
     Module:whereis_name(Name).
+
+-spec reg_name_to_pid(reg_name()) ->
+    pid() | undefined.
+reg_name_to_pid({global, Name}) ->
+    global:whereis_name(Name);
+reg_name_to_pid({via, Module, Name}) ->
+    Module:whereis_name(Name);
+reg_name_to_pid({local, Name})  ->
+    erlang:whereis(Name).
+
+-spec ref_to_pid(ref()) ->
+    pid() | undefined.
+ref_to_pid(Name) when is_atom(Name) ->
+    erlang:whereis(Name);
+ref_to_pid({Name, Node}) when is_atom(Name) andalso is_atom(Node) ->
+    erlang:exit(not_implemented);
+ref_to_pid({global, Name}) ->
+    global:whereis_name(Name);
+ref_to_pid({via, Module, Name}) ->
+    Module:whereis_name(Name);
+ref_to_pid(Pid) when is_pid(Pid) ->
+    Pid.
