dda394d31466b7ac96a6cdda4998ae18a7e66025
Petr Kozorezov <petr.kozorezov@gmail.com>
2018-07-10 18:06:02 +0300

    add some useful lists functions

---
+++ src/genlib_list.erl
@@ -0,0 +1,25 @@
+-module(genlib_list).
+
+%% API
+-export([join/2]).
+-export([compact/1]).
+
+%%
+%% API
+%%
+-spec join(D, list(E)) ->
+    list(D | E).
+join(_    , []   ) -> [];
+join(_    , [H]  ) ->  H;
+join(Delim, [H|T]) -> [H, Delim, join(Delim, T)].
+
+-spec compact(list(undefined | T)) ->
+    list(T).
+compact(List) ->
+    lists:filter(
+        fun
+            (undefined) -> false;
+            (_        ) -> true
+        end,
+        List
+    ).
