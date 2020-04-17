7f9e1ca85b305f3e4c56effe879e188e2ef022f5
Andrew Mayorov <a.mayorov@rbkmoney.com>
2020-04-17 17:04:19 +0300

    Fix wildcard matching multiple stars in tail pos

--- src/genlib_wildcard.erl
+++ src/genlib_wildcard.erl
@@ -13,14 +13,14 @@ match(<<H/utf8, R/binary>>, <<"\\"/utf8, H/utf8, T/binary>>) ->
     match(R, T);
 match(Body = <<_/utf8, Rest/binary>>, Pattern = <<"*"/utf8, T/binary>>) ->
     match(Body, T) orelse match(Rest, Pattern);
+match(Body, <<"*"/utf8, T/binary>>) ->
+    match(Body, T);
 match(<<_/utf8, Rest/binary>>, <<"?"/utf8, T/binary>>) ->
     match(Rest, T);
 match(<<H/utf8, R/binary>>, <<H/utf8, T/binary>>) ->
     match(R, T);
 match(_, <<"\\"/utf8, _/binary>>) ->
     false;
-match(_, <<"*"/utf8>>) ->
-    true;
 match(<<>>, <<>>) ->
     true;
 match(_, _) ->
